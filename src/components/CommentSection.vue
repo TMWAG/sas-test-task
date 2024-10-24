c3fb5c402a066060bd76ac6d1bbd51a1d1ddee42
TMWAG <semyon.valyaev@gmail.com>
2024-10-24 17:31:34 +0300

    finished component, added styles, new function for header value

--- src/components/CommentSection.vue
+++ src/components/CommentSection.vue
@@ -12,14 +12,21 @@ const comments = computed(() => {
   return commentsStore.comments.filter(c => c.postId === props.postId)
 })
 
+const headerText = computed(() => {
+  const lastDigit = Number(comments.value.length.toString().slice(-1))
+  if (lastDigit === 0 || lastDigit >= 5) return 'комментариев'
+  if (lastDigit === 1) return 'комментарий'
+  if (lastDigit >= 1) return 'комментария'
+})
+
 onBeforeMount(async () => {
   commentsStore.getCommentsByPostId(props.postId)
 })
 </script>
 
 <template>
-  <div class="comment_section">
-    <span class="comments_header">{{ comments.length }}</span>
+  <div class="comments_section">
+    <span class="comments_header">{{ comments.length }} {{ headerText }}</span>
     <CommentItem
       v-for="c in comments"
       :key="`comment-${c.id}-${c.postId}-${c.user.id}`"
@@ -28,3 +35,17 @@ onBeforeMount(async () => {
     />
   </div>
 </template>
+
+<style>
+.comments_section {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+.comments_header {
+  font-family: 'SF Pro Display';
+  font-weight: bold;
+  font-size: 28px;
+  margin-top: 40px;
+}
+</style>
