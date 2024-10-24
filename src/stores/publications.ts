3bf7ef5a943bb7517946278826f2048088c5e577
TMWAG <semyon.valyaev@gmail.com>
2024-10-24 13:20:18 +0300

    added check for amount of stored posts, new action getPostById

--- src/stores/publications.ts
+++ src/stores/publications.ts
@@ -6,13 +6,32 @@ export const usePublicationsStore = defineStore('publications', {
   }),
   actions: {
     async getPosts() {
+      if (this.posts.length === 5) {
+        return
+      } 
       try {
         const postsResponse = await fetch(
           'https://dummyjson.com/posts?limit=5',
         ).then<IPostResponse>(res => res.json())
         this.posts = postsResponse.posts
       } catch (e) {
-        console.log(e)
+        console.error(e)
+      }
+    },
+    /**
+     * gets post and puts it in store if it not already present in store
+    */
+    async getPostById(id: number) {
+      if (this.posts.find(p => p.id === id)) {
+        return
+      }
+      try {
+        const postResponse = await fetch(
+          `https://dummyjson.com/posts/${id}`
+        ).then<IPost>(res => res.json())
+        this.posts.push(postResponse)
+      } catch (e) {
+        console.error(e)
       }
     },
     like(postId: number) {
