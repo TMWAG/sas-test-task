cccaf915adac986f6dd0c7c2546646ee1cb73086
TMWAG <semyon.valyaev@gmail.com>
2024-10-24 21:46:58 +0300

    replaced publication components with post components, changed order of imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { onBeforeMount } from 'vue'
 import { usePublicationsStore } from '../stores/publications'
-
-import Publication from '@/components/Publication.vue';
+import Post from '@/components/Post.vue';
 
 const publications = usePublicationsStore()
 onBeforeMount(async() => {
@@ -11,23 +10,18 @@ onBeforeMount(async() => {
 </script>
 
 <template>
-  <div class="pubs_wrapper">
-    <Publication v-for="post in publications.posts"
+  <div class="posts_wrapper">
+    <Post v-for="post in publications.posts"
       :key="`post-${post.id}-${post.title}`"
-      :id="post.id"
-      :title="post.title" 
-      :body="post.body"
-      :reactions="post.reactions"
-      :tags="post.tags"
-      :user-reaction="post.userReaction"
+      :post="post"
       @dislike="publications.dislike(post.id)"
       @like="publications.like(post.id)"
     />
   </div>
 </template>
 
-<style scoped>
-.pubs_wrapper{
+<style>
+.posts_wrapper{
   display: flex;
   flex-direction: column;
   width: 676px;
