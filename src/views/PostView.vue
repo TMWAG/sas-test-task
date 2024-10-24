288340e25eb45f1c1881ac86be7e1af4d4a6c15c
TMWAG <semyon.valyaev@gmail.com>
2024-10-24 16:08:01 +0300

    markup, styles and logic for comments display

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,33 +1,48 @@
 <script setup lang="ts">
-import Publication from '@/components/Publication.vue';
-import { usePublicationsStore } from '@/stores/publications';
+import CommentSection from '@/components/CommentSection.vue'
+import Publication from '@/components/Publication.vue'
+import { usePublicationsStore } from '@/stores/publications'
+import { onMounted, ref } from 'vue'
 
 const props = defineProps<{
   id: string
 }>()
 
-const publications = usePublicationsStore();
+const publications = usePublicationsStore()
+const post = ref<IPost>()
+post.value = publications.posts.find(p => p.id === Number(props.id))
 
-const post = publications.posts.find(p => p.id === Number(props.id))
+onMounted(async () => {
+  if (!post.value) {
+    await publications.getPostById(Number(props.id))
+    post.value = publications.posts[publications.posts.length - 1]
+  }
+})
 </script>
 
 <template>
-<div>
-  <Publication
+  <div
     v-if="post"
-    :body="post.body"
-    :id="post.id"
-    :reactions="post.reactions"
-    :tags="post.tags"
-    :title="post.title"
-    :user-reaction="post.userReaction"
-    @dislike="publications.dislike(post.id)"
-    @like="publications.like(post.id)"
-  />
-  <span v-else>eeee</span>
-</div>
+    class="post_wrapper"
+  >
+    <Publication
+      :body="post.body"
+      :id="post.id"
+      :reactions="post.reactions"
+      :tags="post.tags"
+      :title="post.title"
+      :user-reaction="post.userReaction"
+      @dislike="publications.dislike(post.id)"
+      @like="publications.like(post.id)"
+    />
+    <CommentSection :post-id="post.id" />
+  </div>
 </template>
 
 <style>
-
+.post_wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 676px;
+}
 </style>
